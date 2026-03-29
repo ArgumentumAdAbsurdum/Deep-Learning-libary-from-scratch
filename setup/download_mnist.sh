1d3df5f9526a09b54ea11b5bd6c8285af462df5a
Luca <brandtmarkluca@web.de>
2026-03-29 19:23:55 +0200

    Bugfix: included the download of the test dataset

--- setup/download_mnist.sh
+++ setup/download_mnist.sh
@@ -5,6 +5,9 @@ cd datasets
 URL="https://github.com/phoebetronic/mnist/raw/main/mnist_train.csv.zip"
 FILE="mnist_train.csv.zip"
 
+URL2="https://github.com/phoebetronic/mnist/raw/main/mnist_test.csv.zip"
+FILE2="mnist_test.csv.zip"
+
 if [ ! -f "mnist_train.csv" ]; then
     curl -L -O "$URL"
     unzip -o "$FILE"
@@ -12,3 +15,14 @@ if [ ! -f "mnist_train.csv" ]; then
 else
     echo "Die Datei mnist_train.csv existiert bereits im Ordner 'datasets'."
 fi
+
+
+
+if [ ! -f "mnist_test.csv" ]; then
+    curl -L -O "$URL2"
+    unzip -o "$FILE2"
+    rm "$FILE2"
+else
+    echo "Die Datei mnist_test.csv existiert bereits im Ordner 'datasets'."
+fi
+
